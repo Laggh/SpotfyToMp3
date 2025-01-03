96206f2cb239d654e6a17611f713167f319905ea
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2025-01-03 15:35:21 -0300

    renamed index.js to main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 import youtubedl from "youtube-dl-exec";
-
+const path = "C:/Users/Renan/Documents/GitHub/SpotfyToMp3"; // Path to save the video
 // Download video
 async function downloadVideo(url){
     try {
         const output = await youtubedl(url, {
+            output: path + '/%(title)s.%(ext)s',
             dumpSingleJson: true,
             noCheckCertificates: true,
             noWarnings: true,
@@ -17,4 +18,6 @@ async function downloadVideo(url){
 }
 
 // Call the function
-downloadVideo('https://www.youtube.com/watch?v=6xKWiCMKKJg')
+console.log("Downloading video")
+let output = await downloadVideo('https://www.youtube.com/watch?v=6xKWiCMKKJg')
+console.log(JSON.stringify(output, null, 2))
